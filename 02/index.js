fc64e622a23f65bde1fd383079709d1f87cc6edf
eldergsilva <eldergsilva@gmail.com>
2026-03-09 19:40:25 -0300

    exercício  Encontre o 10

--- 02/index.js
+++ 02/index.js
@@ -10,8 +10,7 @@ for (const letra of letras) {
 }
 
 if (soma < 1) {
-     console.log(`Nenhuma letra "E" ou "e" foi encontrada.`);
-     
+     console.log(`Nenhuma letra "E" ou "e" foi encontrada.`);     
 } else {
 console.log(`Foram encontradas ${soma} letras "E" ou "e".  `);
 }
