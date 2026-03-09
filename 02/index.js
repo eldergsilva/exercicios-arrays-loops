03c756d05d729d73505e42bc5aabba60954726a4
eldergsilva <eldergsilva@gmail.com>
2026-03-09 09:17:13 -0300

    exercicio 02 Contando letras

---
+++ 02/index.js
@@ -0,0 +1,18 @@
+const letras =["A", "e", "B", "C", "E", "z"];
+let soma =0;
+
+for (const letra of letras) {
+    if (letra ==="E" || letra==="e") {
+    soma ++;  
+    console.log();
+           
+    }
+}
+
+if (soma < 1) {
+     console.log(`Nenhuma letra "E" ou "e" foi encontrada.`);
+     
+} else {
+console.log(`Foram encontradas ${soma} letras "E" ou "e".  `);
+}
+
