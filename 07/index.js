1b8988ff3134168361b7f0a7507d2506209528a4
eldergsilva <eldergsilva@gmail.com>
2026-03-11 08:47:00 -0300

    exercicio 07 Filtrando nomes com a letra A

---
+++ 07/index.js
@@ -0,0 +1,15 @@
+const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const listaDeNomes =[]
+ 
+for (const nome of nomes) {
+   if (nome[0] ==="A" || nome[0] ==="a") {
+    listaDeNomes.push(nome)
+   }    
+ 
+   
+    
+} 
+
+
+
+console.log(listaDeNomes);
