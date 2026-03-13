59f79b5f8bb245bf5f36a3ebe9e8b1748a38c064
eldergsilva <eldergsilva@gmail.com>
2026-03-13 09:49:50 -0300

    exercicio 09 Fila de pedidos

---
+++ 10/index.js
@@ -0,0 +1,17 @@
+const numeros = [8, 11, 4, 1];
+let maiorDiferenca = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+    for (let j = 0; j < numeros.length; j++) {
+        console.log(maiorDiferenca);
+        
+        const diferencaAtual = numeros[i] - numeros[j];
+        
+         
+        if (diferencaAtual > maiorDiferenca) {
+            maiorDiferenca = diferencaAtual;
+        }
+    }
+}
+
+console.log(`maiorDiferenca`);
